c97fb3b92af8a231159bc59eb5ec7ec50029a3d2
Mark von der Lieth <mvonderlieth@marinsoftware.com>
2015-10-23 09:34:07 -0700

    Updates

--- ts1.R
+++ ts1.R
@@ -1,3 +1,14 @@
 #http://a-little-book-of-r-for-time-series.readthedocs.org/en/latest/src/timeseries.html
 
 kings <- scan("http://robjhyndman.com/tsdldata/misc/kings.dat",skip=3)
+kingstimeseries = ts(kings)
+
+births <- scan("http://robjhyndman.com/tsdldata/data/nybirths.dat")
+birthstimeseries <- ts(births, frequency=12, start=c(1946,1))
+
+souvenir <- scan("http://robjhyndman.com/tsdldata/data/fancy.dat")
+souvenirtimeseries <- ts(souvenir, frequency=12, start=c(1987,1))
+
+plot.ts(kingstimeseries)
+plot.ts(birthstimeseries)
+plot.ts(souvenirtimeseries)
